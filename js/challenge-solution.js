ecdea1a6908e3ff5df8cd4798f1ab343b85eb6d0
Jason Tsukamoto <jasontsukamoto@gmail.com>
2015-06-11 20:00:10 -1000

    adds test

--- js/challenge-solution.js
+++ js/challenge-solution.js
@@ -1,6 +1,22 @@
 function generate(number) {
-  return [];
-}
+  if(typeof number !== 'number') return false;
+
+  var array = [];
+  var randomArray = [];
+
+  for (var i = 0; i < number; i++) {
+    array.push(i);
+  }
+
+  var length = array.length;
+  for(var k = 0; k < length; k++){
+    var index = (Math.floor((Math.random() * array.length)));
+    randomArray.push(array[index]);
+    array.splice(index,1);
+  }
+  return randomArray;
+ }
+
 
 module.exports = {
   generate : generate
--- spec/test-spec.js
+++ spec/test-spec.js
@@ -9,8 +9,19 @@ describe('This is scrambled num generator', function () {
     scrambled.generate.should.be.a('function');
   });
 
+  it('should return false if parameter is not a number', function () {
+   scrambled.generate({}).should.be.equal(false);
+   scrambled.generate([]).should.be.equal(false);
+   scrambled.generate('string').should.be.equal(false);
+  });
 
   it('should return an array', function () {
-    scrambled.generate().should.be.an('array');
+    scrambled.generate(5).should.be.an('array');
   });
+
+  it('should have a length equal to number', function () {
+    scrambled.generate(1000).length.should.be.equal(1000);
+  });
+
+
 });
